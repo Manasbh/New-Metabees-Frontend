b4d83db393658d2c19f1384edda00b49fc98a9b3
crazyhaller <suvigyamadrid@gmail.com>
2023-10-11 23:13:49 +0530

    add userId in local storage for global use

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,14 +1,10 @@
-import { createContext, useState, useContext, useEffect } from 'react'
+import { createContext, useState, useContext } from 'react'
 import { getCookieInfo } from './getCookie'
 
 export const UserContext = createContext({})
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(getCookieInfo())
-
-  useEffect(() => {
-    localStorage.setItem('user', JSON.stringify(user))
-  }, [user])
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
--- src/utils/getCookie.js
+++ src/utils/getCookie.js
@@ -14,6 +14,7 @@ export function getCookieInfo() {
   try {
     let token = getCookieByName('aToken')
     const decodedToken = jwtDecode(token)
+    localStorage.setItem('userId', decodedToken.id)
     return decodedToken.id
   } catch (e) {}
   return false
